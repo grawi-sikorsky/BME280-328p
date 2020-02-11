c2059f57bdd2d6bd3468072ee16182903e18ec95
Abdul <grawires@gmail.com>
2020-02-11 18:19:32 +0100

    a

--- BME280-328p/src/main.cpp
+++ BME280-328p/src/main.cpp
@@ -5,6 +5,7 @@
 #include <avr/power.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
+#include <digitalWriteFast.h>
 
 #include "LowPower.h"
 
@@ -31,16 +32,20 @@ void printValues() {
     prev_press = press;
     press = bme.readPressure();
 
-    Serial.print("P= ");
-    Serial.println(press / 100.0F);
+    //Serial.print("P= ");
+    //Serial.println(press);
 }
 void checkPress()
 {
-    if(press > prev_press + 10 )
+    if(press > prev_press + 8 )
     {
-        digitalWrite(7,HIGH);
-        delay(60/4);
-        digitalWrite(7,LOW);
+        digitalWriteFast(7,HIGH);
+        digitalWriteFast(13, HIGH);
+        delay(4/4);
+        digitalWriteFast(7,LOW);
+        digitalWriteFast(13, LOW);
+
+        // do RF!
     }
 }
 
@@ -52,67 +57,58 @@ void checkPress()
     MCUSR = 0;     
     // allow changes, disable reset
     WDTCSR = bit (WDCE) | bit (WDE);
-    // set interrupt mode and an interval 
-   // WDTCSR = bit (WDIE) | bit (WDP2) | bit (WDP1);    // set WDIE, and 1 second delay
-    WDT_025s();
-    wdt_reset();  // pat the dog
     
     set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
-    noInterrupts ();           // timed sequence follows
-    sleep_enable();
-    
-    // turn off brown-out enable in software
-    MCUCR = bit (BODS) | bit (BODSE);
-    MCUCR = bit (BODS); 
-    interrupts ();             // guarantees next instruction executed
     sleep_cpu ();
  }
 
 void setup() {
     Serial.begin(9600);
-    clock_prescale_set(clock_div_4);
+    //clock_prescale_set(clock_div_1);
     
     for (byte i = 0; i <= A5; i++)
     {
-        pinMode (i, OUTPUT);    // changed as per below
-        digitalWrite (i, LOW);  //     ditto
+        pinModeFast(i, OUTPUT);    // changed as per below
+        digitalWriteFast(i, LOW);  //     ditto
     }
+    // setup pin 12 as a DigitalOut and turn it off
 
-    digitalWrite(13, LOW); // LED OFF
+    digitalWriteFast(13, LOW); // LED OFF
 
     power_adc_disable(); // ADC converter
     power_spi_disable(); // SPI
-    //power_usart0_disable();// Serial (USART)
+    power_usart0_disable();// Serial (USART)
     //power_timer0_disable();// Timer 0
     power_timer1_disable();// Timer 1
-    //power_timer2_disable();// Timer 2
+    power_timer2_disable();// Timer 2
     //power_twi_disable(); // TWI (I2C)
 }
 
 
 
 void loop() {
-    bme.begin(0x76);
+    //power_adc_disable(); // ADC converter
+    //power_spi_disable(); // SPI
+    //power_usart0_disable();// Serial (USART)
+    //power_timer0_disable();// Timer 0
+    //power_timer1_disable();// Timer 1
+    //power_timer2_disable();// Timer 2
+    //power_twi_enable(); // TWI (I2C)
 
+    bme.begin(0x76);
     printValues();
     checkPress();
 
-    //delay(1);
-    digitalWrite(13, HIGH);
-    delay(20/4);
-    digitalWrite(13, LOW);
-    //delay(50/8);
-
-    power_adc_disable(); // ADC converter
-    power_spi_disable(); // SPI
+    //power_adc_disable(); // ADC converter
+    //power_spi_disable(); // SPI
     //power_usart0_disable();// Serial (USART)
     //power_timer0_disable();// Timer 0
-    power_timer1_disable();// Timer 1
-    power_timer2_disable();// Timer 2
+    //power_timer1_disable();// Timer 1
+    //power_timer2_disable();// Timer 2
     //power_twi_disable(); // TWI (I2C)
 
     //LowPower.idle(SLEEP_1S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    LowPower.powerDown(SLEEP_250MS, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
 
     //gotosleep();
     
