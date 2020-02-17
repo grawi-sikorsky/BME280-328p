3d63394d45a2da70a291bcece1fb132f5b1a10f2
Abdul <grawires@gmail.com>
2020-02-17 17:28:30 +0100

    adasensor - out / poprawku

--- BME280-328p/src/main.cpp
+++ BME280-328p/src/main.cpp
@@ -6,7 +6,6 @@
 #include <digitalWriteFast.h>
 #include "LowPower.h"
 
-#define TINYBME
 //#define VWRF
 
 #ifdef VWRF
@@ -16,21 +15,12 @@
   #include <SPI.h> // Not actually used but needed to compile
 #endif
 
-#ifdef TINYBME
-  #define TINY_BME280_I2C
-  #include "TinyBME280.h"
-  #define SENSE_VALUE 30
-#else
-  #include <Adafruit_Sensor.h>
-  #include "Adafruit_BME280.h"
-  #define SENSE_VALUE 1
-#endif
+#define TINY_BME280_I2C
+#include "TinyBME280.h"
+#define SENSE_VALUE 30
 
-#ifdef TINYBME
-  tiny::BME280 bme1; //Uses I2C address 0x76 (jumper closed)
-#else
-  Adafruit_BME280 bme; // I2C
-#endif
+
+tiny::BME280 bme1; //Uses I2C address 0x76 (jumper closed)
 
 float press, prev_press;
 time_t current_positive, last_positive, // czas ostatniego dmuchniecia
@@ -43,15 +33,16 @@ bool was_whistled;                      // flaga dmuchniete czy nie
 #define SPEAKER_PIN     7
 #define TRANSMISION_PIN 4
 
-RH_ASK rfsender;
+RH_ASK rfsender(2000,12,11,3,true);
 
 void readValues();
 void checkTimeout();
 
+
 void wykonaj_transmisje()
 {
-  uint8_t temperature = 10;
-
+  const char *msg = "JEB";
+ 
   #ifdef VWRF
     digitalWrite(LED_PIN, HIGH); // Flash a light to show transmitting
     vw_send((uint8_t *)&temperature, sizeof(temperature));
@@ -60,14 +51,14 @@ void wykonaj_transmisje()
     delay(500);
   #else
     power_timer1_enable();
-    digitalWriteFast(LED_PIN, HIGH); // Flash a light to show transmitting
+    //digitalWriteFast(LED_PIN, HIGH); // Flash a light to show transmitting
     
-    rfsender.send((uint8_t *)temperature, sizeof(temperature));
+    rfsender.send((uint8_t *)msg, sizeof(msg));
     rfsender.waitPacketSent();
 
-    digitalWriteFast(LED_PIN, LOW);
+    //digitalWriteFast(LED_PIN, LOW);
     power_timer1_disable();
-    delay(20);
+    delay(2);
   #endif
 }
 
@@ -83,22 +74,27 @@ void setup_rf()
   #endif
 }
 
-
+/*****************************************************
+ * SETUP
+ * ***************************************************/
 void setup() {
-  //Serial.begin(9600);
   //clock_prescale_set(clock_div_2);
 
   ADCSRA &= ~(1 << 7); // TURN OFF ADC CONVERTER
-
-  #ifdef TINYBME
-    bme1.setI2CAddress(0x76);
-    bme1.beginI2C();
-  #endif
+  power_adc_disable(); // ADC converter
+  power_spi_disable(); // SPI
+  power_usart0_disable();// Serial (USART)
+  //power_timer0_disable();// TIMER 0 SLEEP WDT ...
+  //power_timer1_disable();// Timer 1 - I2C...
+  power_timer2_disable();// Timer 2
+  
+  bme1.setI2CAddress(0x76);
+  bme1.beginI2C();
 
   for (byte i = 0; i <= A5; i++)
   {
     pinModeFast(i, OUTPUT);    // changed as per below
-    digitalWriteFast(i, HIGH);  //     ditto
+    digitalWriteFast(i, LOW);  //     ditto
   }
   digitalWriteFast(LED_PIN, LOW);  // LED OFF
   digitalWriteFast(SPEAKER_PIN, LOW);    // SPK
@@ -106,38 +102,33 @@ void setup() {
 
   readValues();               // pierwsze pobranie wartosci - populacja zmiennych
   setup_rf();
-
-  power_adc_disable(); // ADC converter
-  power_spi_disable(); // SPI
-  power_usart0_disable();// Serial (USART)
-  //power_timer0_disable();// Timer 0
-  power_timer1_disable();// Timer 1
-  power_timer2_disable();// Timer 2
 }
 
+/*****************************************************
+ * Pobiera dane z czujnika BME280
+ * ***************************************************/
 void readValues() {
   prev_press = press;
 
-  #ifdef TINYBME
-    press = bme1.readFixedPressure();
-  #else
-    press = bme.readPressure();
-  #endif
+  power_twi_enable();
+  press = bme1.readFixedPressure();
+  power_twi_disable();
 }
 
-void checkPressure()
-{
+/*****************************************************
+ * Sprawdza czy cisnienie jest wieksze od zalozonego
+ * ***************************************************/
+void checkPressure(){
   if(press > prev_press + SENSE_VALUE )   // jeśli nowy odczyt jest wiekszy o SENSE_VALUE od poprzedniego ->
   {
     was_whistled = true;
-
     wykonaj_transmisje();
-
-    digitalWriteFast(SPEAKER_PIN, HIGH);   // SPK
-    delayMicroseconds(100);
-    digitalWriteFast(SPEAKER_PIN, LOW);    // SPK
   }
 }
+
+/*****************************************************
+ * TEST - TIMEOUT
+ * ***************************************************/
 void checkTimeout()
 {
   if(current_positive - last_positive > TIME_TO_WAIT_MS) 
@@ -149,55 +140,50 @@ void checkTimeout()
   else if(current_positive - last_positive > TIMEOUT_1 )
   {
     // zmniejsz probkowanie 1x/s
-  }
+    //LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+  } 
   else if(current_positive - last_positive > TIMEOUT_2 )
   {
     // przejsc w deep sleep
   }
 }
-// Brown-out disable // ->  avrdude -c usbtiny -p m328p -U efuse:w:0x07:m
-
 
 
 /**************************
  *  LOOP
  * ************************/
 void loop() {
-  //Serial.begin(9600);
 
-  #ifdef TINYBME
-    bme1.beginI2C();
-    //bme1.reset();
-  #else
-    bme.begin(0x76);
-  #endif
-
-  if(was_whistled == false)       // jeżeli poprzednio nie było dmuchnięcia
+  if(was_whistled == false)  // jeżeli poprzednio nie było dmuchnięcia
   {
-    readValues();
-    checkPressure();
+    power_timer1_enable();
+    power_twi_enable();
+      bme1.beginI2C();
+      readValues();
+      checkPressure();
+    power_twi_disable();
+    power_timer1_disable();
+
+    digitalWriteFast(I2C_SCL_PIN, LOW);
+    digitalWriteFast(I2C_SDA_PIN, LOW);
+    ADCSRA &= ~(1 << 7);
   }
-  else                            // jeżeli poprzednio było dmuchnięcie
-  {
+  else                      // jeżeli poprzednio było dmuchnięcie
+  {                         // TIME_TO_WAIT_MS okresla czas przerwy
+    power_twi_disable();
+    
     current_positive = millis();  // pobierz czas.
-      digitalWriteFast(LED_PIN, HIGH);  // LED OFF
-      delayMicroseconds(100);
-      digitalWriteFast(LED_PIN, LOW);  // LED OFF
+
     if(current_positive - last_positive > TIME_TO_WAIT_MS) 
     {
       // save the last time you blinked the LED
       last_positive = current_positive;
-
-      //digitalWriteFast(SPEAKER_PIN, HIGH);   // SPK
-      digitalWriteFast(LED_PIN, HIGH);  // LED OFF
-      delayMicroseconds(100);
-      //digitalWriteFast(SPEAKER_PIN, LOW);    // SPK
-      digitalWriteFast(LED_PIN, LOW);  // LED OFF
-
       was_whistled = false;
     }
+    power_timer1_disable();
   }
 
+  checkTimeout();
 
   LowPower.powerDown(SLEEP_250MS, ADC_OFF, BOD_OFF);
 }
